98038d437ce41ae6b149977ec3a43a36c40c3daf
Jason <kexing.ying19@imperial.ac.uk>
2019-11-18 21:40:15 +0000

    initial commit

---
+++ M40001_3.lean
@@ -0,0 +1,29 @@
+-- begin header
+import tactic.ring
+import data.real.basic
+import data.setoid
+import tactic.linarith
+import tactic.norm_cast
+
+namespace M40001_3
+-- end header
+
+/-Section
+2.5 Binary Relations
+-/
+
+variable {X : Type}
+def reflexive (r : setoid X) := ∀ x : X, r.rel x x → true
+def symmetric (r : setoid X) := ∀ x y : X, r.rel x y → r.rel y x
+def antisymmetric (r : setoid X) := ∀ x y : X, r.rel x y ∧ r.rel y x → x = y
+def trainsitive (r : setoid X ) := ∀ x y z : X, r.rel x y ∧ r.rel y z → r.rel x z
+
+def partial_order (r : setoid X) := reflexive r ∧ symmetric r ∧ transitive r.rel
+def total (r : setoid X) := ∀ x y, r.rel x y ∨ r.rel y x
+def total_order (r : setoid X) := partial_order r ∧ total r
+
+def equivalence (r : setoid X) := reflexive r ∧ symmetric r ∧ transitive r.rel
+
+
+
+end M40001_3

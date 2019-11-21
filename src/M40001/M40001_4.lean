bfafc37c9eb5c14a2cc44a1c6c99d91bceb893be
Jason <kexing.ying19@imperial.ac.uk>
2019-11-21 21:20:20 +0000

    initial commit

---
+++ src/M40001/M40001_4.lean
@@ -0,0 +1,15 @@
+-- begin header
+import tactic.ring
+import data.real.basic
+import tactic.linarith
+import tactic.norm_cast
+
+namespace M40001_4
+-- end header
+
+/- Section
+2.9 Quotients and Equivalence Classes
+-/
+
+
+end M40001_4

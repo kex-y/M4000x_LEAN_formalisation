61982818ac02c4b2fbf533ef49b9bfc7f4caf138
Jason <kexing.ying19@imperial.ac.uk>
2019-11-29 23:07:52 +0000

    goal 2 complete

--- src/M40001/Partition_iso_equiv_class.lean
+++ src/M40001/Partition_iso_equiv_class.lean
@@ -5,7 +5,7 @@ namespace M40001
 
 def tricky (X : Type*) : {R : bin_rel X // equivalence R} ≃ {A : set (set X) // partition A} :=
 { to_fun := λ r, ⟨{a : set X | ∃ s : X, a = cls r.1 s}, equiv_relation_partition r.1 r.2⟩,
-  inv_fun := λ a, ⟨rs a, by {apply partition_equiv_relation, from a.2} ⟩,
+  inv_fun := λ a, ⟨rs a.1, partition_equiv_relation a.1 a.2 ⟩,
   left_inv := begin sorry end,
   right_inv := begin sorry end
 }

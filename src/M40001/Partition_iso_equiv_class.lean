e2efb557f01865691f7d495e653333044928aef2
Jason <kexing.ying19@imperial.ac.uk>
2019-11-29 21:04:11 +0000

    added proof to inv_fun

--- src/M40001/Partition_iso_equiv_class.lean
+++ src/M40001/Partition_iso_equiv_class.lean
@@ -5,7 +5,9 @@ namespace M40001
 
 def tricky (X : Type*) : {R : bin_rel X // equivalence R} ≃ {A : set (set X) // partition A} :=
 { to_fun := λ r, ⟨{a : set X | ∃ s : X, a = cls r.1 s}, equiv_relation_partition r.1 r.2⟩,
-  inv_fun := _,
+  inv_fun := λ a, ⟨rs a, by {apply partition_equiv_relation, from a.2} ⟩,
   left_inv := begin sorry end,
   right_inv := begin sorry end
 }
+
+end M40001

ac13721ac0bb8b97f20199bc856ac9ab90da228f
Jason <kexing.ying19@imperial.ac.uk>
2019-12-12 21:56:23 +0000

    more sequences

--- src/M40002/M40002_C3.lean
+++ src/M40002/M40002_C3.lean
@@ -40,5 +40,30 @@ begin
         {rwa hc}
 end
 
+-- Example 3.5
+example (a : ℕ → ℝ) (ha : a = λ n : ℕ, (n + 5) / (n + 1)) : a ~> 1 :=
+begin
+    intros ε hε,
+    have : ∃ N : ℕ, (4 / N : ℝ) < ε :=
+        by {cases exists_nat_one_div_lt hε with M hM,
+        use (4 * (M + 1)),
+        suffices : 4 / (4 * (↑M + 1)) < ε,
+          exact_mod_cast this,
+        have : (4 : ℝ) ≠ 0 := by linarith,
+        rwa (div_mul_right (↑M + 1 : ℝ) this),
+        },
+    cases this with N hN,
+    use N, intros n hn,
+    have h0 : 0 ≤ a n - 1 :=
+        by {rw ha, simp,
+        rw (show (5 + ↑n :ℝ) = 4 + 1 + ↑n, by linarith),
+        rw [add_assoc, add_div, div_self], 
+        suffices : (0 : ℝ) ≤ 4 / (1 + ↑n),
+          simpa using this,
+        refine le_of_lt (div_pos (by linarith) _),
+        repeat {norm_cast, linarith},
+        }
+end
+
 
 end M40002

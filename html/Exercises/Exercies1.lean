62b0636033ead140c7a4572b2cdaa6f8aa4eb535
Jason <kexing.ying19@imperial.ac.uk>
2020-01-25 00:35:58 +0000

    initial commit

---
+++ html/Exercises/Exercies1.lean
@@ -0,0 +1,11 @@
+/-
+Exercise 1.
+Try to prove that ¬ P ↔ (P → false) without using automation!
+
+Here are some basic tactics that might help : http://wwwf.imperial.ac.uk/~buzzard/xena/html/source/tactics/guide.html
+-/
+
+example (P : Prop) : ¬ P ↔ (P → false) :=
+begin
+    sorry -- Replace the sorry with your proof!
+end

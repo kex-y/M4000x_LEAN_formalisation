2da5cfef453dcbaa142a1101bdf87c489002bec6
Jason <kexing.ying19@imperial.ac.uk>
2020-01-25 00:51:35 +0000

    initial commit

---
+++ html/Exercises/Exercies2.lean
@@ -0,0 +1,13 @@
+/-
+Exercise 2.
+What is the contrapositive of ¬ Q → ¬ P
+-I am suggesting P → Q is the contrapositive of ¬ Q → ¬ P. Is this correct? 
+Prove that it is correct if you agree with me, otherwise change P → Q into what ever your heart desires!
+
+Here are some basic tactics that might help : http://wwwf.imperial.ac.uk/~buzzard/xena/html/source/tactics/guide.html
+-/
+
+example (P Q : Prop) : (¬ Q → ¬ P) → (P → Q) :=
+begin
+    sorry -- Replace the sorry with your proof!
+end

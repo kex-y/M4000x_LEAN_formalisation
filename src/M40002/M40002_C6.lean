16b134056979982ea2e8942b220da5bde83b0ff5
JasonKYi <kexing.ying19@imperial.ac.uk>
2020-01-31 19:25:46 +0000

    definition of differentiablility

--- src/M40002/M40002_C6.lean
+++ src/M40002/M40002_C6.lean
@@ -4,6 +4,9 @@ import M40002.M40002_C5
 
 namespace M40002
 
+-- Definition of a function being differntiable at a point
+def differentiable_at (f : ℝ → ℝ) (a : ℝ) := ∃ l : ℝ, func_converges_to (λ x : ℝ, (f x - f a) / (x - a)) a l
+def differentiable (f : ℝ → ℝ) := ∀ a : ℝ, differentiable_at f a
 
 
 end M40002

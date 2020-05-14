c2c551304292ec61f8a11eb5f5821b0c87833119
JasonKYi <kexing.ying19@imperial.ac.uk>
2020-05-14 21:04:01 +0100

    update

--- src/M40002/countability.lean
+++ src/M40002/countability.lean
@@ -25,10 +25,6 @@ See nat.find
 
 open function M40001
 
-lemma no_nat_lt_zero : ¬ (∃ x : ℕ, x < 0) := by simp
-
-lemma nat_le_imp_lt_succ (k : ℕ) : ∀ (x : ℕ), x ≤ k ↔ x < k + 1 := λ _, by rwa nat.lt_succ_iff
-
 /- Theorem
 The Well-Ordered Principle. If $S$ is a set of natural numbers and is non-empty, then there exists an element $n ∈ S, ∀s ∈ S, n ≤ s$.
 -/
@@ -50,9 +46,8 @@ begin
         rw hb at hc,
 -- But if $0 ∈ S$ then $S$ has a minimum, $0$ can't be in $S$, implying $0 ∈ B$.
         have : ∃ (s : ℕ), s ∈ S ∧ s < 0 := by {apply ha 0, assumption},
-        rcases this with ⟨y, ⟨hd, he⟩⟩,
-        have : ∃ y : ℕ, y < 0 := by {use y, assumption},
-        from no_nat_lt_zero this
+        rcases this with ⟨y, ⟨_, he⟩⟩,
+        refine not_lt_of_ge _ he, norm_num,
         },
 -- Now assume $k ∈ B$ for some $k ∈ ℕ$, then we need to prove $k + 1 ∈ B$, i.e. we need to prove $∀ x ∈ ℕ, x ≤ k + 1 → x ∉ S$.
         {rw set.mem_set_of_eq at hk,
@@ -60,7 +55,7 @@ begin
 -- Let $x$ be an arbitary natural number thats less than $k + 1$. Suppose $x ∈ S$, we will show a contradiction.
         intros x hx hb,
 -- As $x ≤ k ⇔ x < k + 1$, we have $x < k + 1 → x ∉ S$.
-        have hl : x < k + 1 → x ∉ S := by {rwa ←nat_le_imp_lt_succ, from hk x},
+        have hl : x < k + 1 → x ∉ S := by {rwa nat.lt_succ_iff, from hk x},
 -- Then as we have assumed $x ∈ S$, $x ≥ k + 1$ since $x < k + 1 → x ∉ S$.
         have : x = k + 1 := 
             by {cases lt_trichotomy x (k + 1),
@@ -70,18 +65,15 @@ begin
 -- But we have $x < k + 1$, thus, by the trichotomy axiom, $x = k + 1$.
                     {have : k + 1 < x ∧ x ≤ nat.succ k := ⟨h_1, hx⟩, 
                     revert this, simp
-                    }
-                }
-            },
+                    } } },
 -- Then, as $S$ has no least element, there is some $s ∈ S, s < k + 1$.
         rw this at hb,
         have hc : ∃ (s : ℕ), s ∈ S ∧ s < k + 1 := by {apply ha, assumption},
         rcases hc with ⟨y, ⟨hd, he⟩⟩,
 -- But as $k ∈ B$, $s < k + 1 ⇒ s ∉ S$, we have a contradiction! Therefore, by mathematical induction, we have $B = ℕ$
         apply hk y,
-        rwa nat_le_imp_lt_succ, assumption
-        }
-        },
+        rwa ←nat.lt_succ_iff, assumption
+        } },
 -- As, by construction, we have $S$ and $B$ are disjoint. Thus, since $S ⊆ ℕ, B = ℕ ⇒ S = ∅$.
     apply h,
     have hSempty : ∀ n ∈ B, n ∉ S := 
